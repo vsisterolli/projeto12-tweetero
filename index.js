8ea605b32bfdc25368a217cd05767a73ce5b50fb
vsisterolli <vsisterolli@gmail.com>
2022-11-07 06:20:17 -0300

    Finished bonus functionalities

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { query } from "express"
 import cors from "cors"
 
 const app = express()
@@ -9,33 +9,44 @@ const users = []
 const tweets = []
 
 app.post("/sign-up", (req, res) => {
-    console.log(req.body)
     if(!req.body.username || !req.body.avatar) {
-        res.status(400).send({ message: "Insira todos os campos por favor."});
+        res.status(400).send("Todos os campos são obrigatórios!");
         return;
     }
-    users.push({...req.body})
-    res.send("OK")
+    users.push({...req.body});
+    res.status(201).send("OK");
 })
 
 app.post("/tweets", (req, res) => {
-    if(!req.body.username || !req.body.tweet) {
-        res.status(400).send({ message: "Corpo de requisição inválido." });
+
+    if(!req.headers.user || !req.body.tweet) {
+        res.status(400).send("Todos os campos são obrigatórios!");
         return;
     }
-    tweets.push({...req.body})
-    res.send("OK")
+    tweets.push({
+        "username": req.headers.user,
+        "tweet": req.body.tweet
+    })
+    res.status(201).send("OK");
 })
 
 app.get("/tweets", (req, res) => {
-    const lastTweets = []
-    for(let i = tweets.length-1, k = 0; i >= 0 && k < 10; k++, i--)
+
+    console.log(req.query.page)
+    const lastTweets = [];
+
+    for(let i = tweets.length-1 - 10 * (req.query.page - 1), k = 0; i >= 0 && k < 10 * req.query.page; k++, i--)
         lastTweets.push({
             "username": tweets[i].username,
             "avatar": users.find(value => value.username === tweets[i].username).avatar,
             "tweet": tweets[i].tweet,
         })
-    res.send(lastTweets)
+    res.send(lastTweets);
+
+})
+
+app.get("/tweets/:username", (req, res) => {
+    res.send(tweets.filter(value => value.username === req.params.username).reverse());
 })
 
 app.listen(5000)
